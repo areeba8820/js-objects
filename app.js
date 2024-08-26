adec01ae6bfdacb0dfb53fd2ea0dd70cecf055a2
Areeba Huzaifa <162459943+areeba8820@users.noreply.github.com>
2024-08-26 15:18:09 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+function myFunction(){
+    let node = document.createElement("li");
+    let myList = document.createTextNode("Water");
+    node.appendChild(myList);
+    document.getElementById("List").appendChild(node);
+}
+
+
+
+
+// function myFunction(){
+//     const list = document.getElementById("List");
+//     list.removeChild(list.firstElementChild)
+// }
+
+
+let plan1 = {
+    name : "Basic",
+    price : 3.99,
+    space : 100,
+    transfer : 1000,
+    pages : 10
+}
+plan1.price = 455;
+
+console.log(plan1);
+
+
+let plan2 = {
+    name : "Professional",
+    price : 5.99,
+    space : 500,
+    transfer : 5000,
+    pages : 50
+}
+// plan2.space = 600;
+
+console.log(plan2);
+
+
+let plan3 = {
+    name : "Ultimate",
+    price : 9.99,
+    space : 2000,
+    transfer : 20000,
+    pages : 500
+}
+plan1.price = 455;
+plan1.pages = 30;
+
+// console.log(plan1.name[1]);
+
+// console.log(plan1.email);
+
+delete plan1.space;
+console.log(plan1);
+
+console.log(plan3);
+// alert("The cost of the " + plan1.name + " package is $" + plan1.price + " per month.")
+
+
+
